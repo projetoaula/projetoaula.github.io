d15451bd69e2e81d1871b0ab9d9d7b356b3120f3
deva0o1 <devairs.ju@gmail.com>
2026-04-23 20:17:54 -0300

    primeiro comit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [] = useState(0)
+
+  return (
+    <>
+      <h1>ola, mundo, meu nome e deva</h1>
+    </>
+  )
+}
+
+export default App
